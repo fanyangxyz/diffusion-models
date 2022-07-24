a6a92999ae6516b7d574343ab71aacf8aa2f3b91
Fan Yang <iamyangfan930@gmail.com>
2022-07-24 13:22:29 -0700

    print the number of model parameters

--- ddpm/unet.py
+++ ddpm/unet.py
@@ -356,6 +356,9 @@ class UNet(nn.Module):
         self.out_norm = get_norm(norm, base_channels, num_groups)
         self.out_conv = nn.Conv2d(base_channels, img_channels, 3, padding=1)
 
+        print("number of parameters: %e", sum(p.numel()
+              for p in self.parameters()))
+
     def forward(self, x, time=None, y=None):
         ip = self.initial_pad
         if ip != 0:
